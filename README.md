7f94ea24a499ceff2a5bcf552b933f9ed370a7ce
luoxuwei <xwluo@foxmail.com>
2021-08-03 22:26:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 介绍
+
+封装WebView与Native交互的框架，支持组件化，各个组件给WebView提供的接口可以在自己模块里实现，定义好与WebView交互的协议即可，框架自动注册分发，解耦JavaScript Interface具体业务逻辑与WebView的依赖。
---
+++ app/src/main/java/com/luoxuwei/jsrouter/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.luoxuwei.jsrouter.demo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by xuwei.luo on 2021/8/4.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
