171a358a3556a5415878be19e4dc2bd23cfb19b5
luoxuwei <xwluo@foxmail.com>
2021-08-14 18:15:58 +0800

    创建gradle plugin项目

---
+++ arouter-gradle-plugin/src/main/groovy/com/luoxuwei/jsrouter/register/PluginLaunch.groovy
@@ -0,0 +1,15 @@
+package com.luoxuwei.jsrouter.register
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by 罗旭维 on 2021/8/14.
+ */
+public class PluginLaunch implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
