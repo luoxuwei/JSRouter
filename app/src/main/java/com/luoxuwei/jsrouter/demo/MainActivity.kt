43106c4afeb318d45ae9cbf5fcc1297effe1d80a
luoxuwei <xwluo@foxmail.com>
2021-08-16 00:18:54 +0800

    修复demo中的bug

--- app/src/main/java/com/luoxuwei/jsrouter/demo/MainActivity.kt
+++ app/src/main/java/com/luoxuwei/jsrouter/demo/MainActivity.kt
@@ -19,30 +19,11 @@ class MainActivity : AppCompatActivity() {
         val settings: WebSettings = webView.getSettings()
         settings.javaScriptEnabled = true
         settings.setSupportMultipleWindows(true)
-        settings.domStorageEnabled = true
-        settings.javaScriptCanOpenWindowsAutomatically = true
-        settings.pluginState = WebSettings.PluginState.ON // 设置插件支持
 
         settings.useWideViewPort = true
         settings.loadWithOverviewMode = true
         settings.setSupportZoom(false) // 设置支持缩放
-
         settings.builtInZoomControls = false
-        settings.cacheMode = WebSettings.LOAD_DEFAULT // 设置默认缓存模式，根据cache-control决定是否从网络上取数据。
-
-        settings.databaseEnabled = true // 启用数据库缓存
-
-        settings.domStorageEnabled = true // 启用DOM缓存
-
-        settings.setAppCacheEnabled(true) // 启用应用缓存
-
-        settings.savePassword = false // 关闭webview的自动保存密码
-
-        settings.allowContentAccess = true
-        settings.allowFileAccess = false // 启用WebView访问文件数据
-
-        settings.allowUniversalAccessFromFileURLs = false //允许跨域
-
         settings.textZoom = 100 //设置字体大小不变
 
         webView.setVerticalScrollBarEnabled(false)
@@ -51,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         jsRouterInject = JSRouterInject.Builder(this, webView).setUrl("").build()
 
         webView.loadUrl("file:///android_asset/test.html")
-        jsRouterInject?.injectJs()
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
